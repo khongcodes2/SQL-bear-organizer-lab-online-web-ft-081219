4a9d6d068b2c938bda06028ab5ee64a9ea5964fd
KHong <albtrossfacefall@gmail.com>
2019-09-18 12:09:45 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Mr. Chocolate',4,'M','Brown','Sweet',true);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Rowdy',2,'F','Black','Playful',true);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Tabitha',8,'F','Brown','Gentle',true);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Sergeant Brown',10,'F','Black','Lazy',true);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Melissa',13,'F','Brown','Cheerful',true);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Grinch',3,'M','Polka-dotted','Greedy',true);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Wendy',5,'F','Orange','Sly',true);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES (NULL,8,'M','Black','Violent',true);
